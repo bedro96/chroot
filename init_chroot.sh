d9550c792d73aaa104935606b4da5a7406aa1144
Ubuntu <kunhoko@ubuntu1604110.ev52cbvww2jetdpz11fss4s3lf.ix.internal.cloudapp.net>
2019-11-26 06:14:12 +0000

    update init_chroot.sh

--- init_chroot.sh
+++ init_chroot.sh
@@ -4,4 +4,5 @@ export ARM_CLIENT_ID=f5202f78-5383-4463-9d2d-4d88461c02cb
 export ARM_CLIENT_SECRET=580afef1-8dfe-4858-9a01-72f503c27420
 export ARM_SUBSCRIPTION_ID=05be085b-86ea-4336-addc-38fd56051a9e
 export ARM_RESOURCE_GROUP=imgRepoRG
+export ARM_SOURCE_DISK_RESOURCE_ID=/subscriptions/05be085b-86ea-4336-addc-38fd56051a9e/resourceGroups/terraformrg/providers/Microsoft.Compute/disks/ubuntu1604110-osdisk
 
